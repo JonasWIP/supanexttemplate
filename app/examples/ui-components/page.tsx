ed372ff3cb0d1ae5f4ff46af20d9e0573b0a7ee0
JonasWIP <dev.jonas.reitz@gmail.com>
2025-05-01 14:05:28 +0200

    fix: update apostrophes to HTML entities in Accordion content

--- app/examples/ui-components/page.tsx
+++ app/examples/ui-components/page.tsx
@@ -395,13 +395,13 @@ export default function UIComponentsPage() {
               <AccordionItem value="item-2">
                 <AccordionTrigger>Is it styled?</AccordionTrigger>
                 <AccordionContent>
-                  Yes. It comes with default styles that match the other components' aesthetic.
+                  Yes. It comes with default styles that match the other components&apos; aesthetic.
                 </AccordionContent>
               </AccordionItem>
               <AccordionItem value="item-3">
                 <AccordionTrigger>Is it animated?</AccordionTrigger>
                 <AccordionContent>
-                  Yes. It's animated by default, but you can disable it if you prefer.
+                  Yes. It&apos;s animated by default, but you can disable it if you prefer.
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
